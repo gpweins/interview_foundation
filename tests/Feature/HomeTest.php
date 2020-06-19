6b41886ca476572490179dc98df39e41e97826cd
Gustavo Weinschutz <gpweins@gmail.com>
2020-06-19 16:04:29 -0300

    Add basic tests for authentication

---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\User;
+
+class HomeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_authenticated_user_can_see_welcome_page()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get('/');
+
+        $response->assertSee('Laravel');
+        $response->assertSee('Home');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_authenticated_user_can_see_home_page()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get('/home');
+
+        $response->assertSee('Laravel');
+        $response->assertSee($user->name);
+        $response->assertSee('Logout');
+        $response->assertSee('You are logged in!');
+
+        $response->assertStatus(200);
+    }
+}
--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -5,17 +5,20 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class WelcomeTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function test_guest_user_can_see_welcome_page()
     {
         $response = $this->get('/');
 
+        $response->assertSee('Login');
+        $response->assertSee('Register');
+        $response->assertSee('Laravel');
         $response->assertStatus(200);
     }
 }
